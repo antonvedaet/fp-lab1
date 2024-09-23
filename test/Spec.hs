a30e215a7d3a6daafc529d71af785eb1ef6867b7
AntonSexov <antonvedernikov2003@gmail.com>
2024-09-23 18:10:34 +0300

    feat: testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.HUnit
+import Lib
+
+
+testTask13 :: Test
+testTask13 = TestCase (assertEqual "Expected task13 to return '5537376230'" "5537376230" (task13Solution))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    _ <- runTestTT $ TestList [testTask13]
+    return ()
