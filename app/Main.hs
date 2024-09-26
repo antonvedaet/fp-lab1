efc0b6ed389c4f1fe2299ba5095c460139f3db88
anton <antonvedernikov2003@gmail.com>
2024-09-26 11:55:48 +0300

    linting main

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main (main) where
 import Lib (takeFirst10Digits, task13Numbers, task13Recursively)
 
 main :: IO ()
-main = putStrLn $ show (takeFirst10Digits $ task13Recursively task13Numbers)
+main = print $ takeFirst10Digits $ task13Recursively task13Numbers
